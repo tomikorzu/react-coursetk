a501054d03794d548d6894633949d935137a957b
tomikorzu <tomykorzu@icloud.com>
2024-09-16 12:40:20 -0300

    fix(NavbarMenu.jsx): add structure

--- vite/src/components/NavbarMenu.jsx
+++ vite/src/components/NavbarMenu.jsx
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../assets/navbar.css";
 
-const NavbarMenu = () => {
+const NavbarMenu = ({ items }) => {
+  const navigate = useNavigate();
+
+  const handleLinkButton = (url) => {
+    navigate(url);
+  };
+
+  useEffect(() => {
+    const toggleBtn = document.querySelector(".toggle-btn");
+    const handleToggle = () => {
+      toggleBtn.classList.toggle("active");
+      const navbar = document.querySelector(".navbar");
+      navbar.classList.toggle("clip");
+    };
+
+    toggleBtn.addEventListener("click", handleToggle);
+
+    return () => {
+      toggleBtn.removeEventListener("click", handleToggle);
+    };
+  }, []);
+
   return (
     <nav className="navbar">
+      <button className="toggle-btn">
+        <span className="line line1"></span>
+        <span className="line line2"></span>
+        <span className="line line3"></span>
+      </button>
       <ul className="navbar-menu">
-        <li className="navbar-menu-item">
-          <a href="/">Home</a>
-        </li>
-        <li className="navbar-menu-item">
-          <a href="/classes">Classes</a>
-        </li>
-        <li className="navbar-menu-item">
-          <a href="/about">About</a>
-        </li>
+        {items.map((item, index) => (
+          <li key={index} className="navbar-item">
+            <button
+              className="navbar-link"
+              onClick={() => handleLinkButton(item.url)}
+            >
+              {item.item}
+            </button>
+          </li>
+        ))}
       </ul>
     </nav>
   );
