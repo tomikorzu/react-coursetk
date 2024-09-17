c36e93e48f8df04af13617a8b3d65892fde1f794
tomikorzu <tomykorzu@icloud.com>
2024-09-17 11:38:11 -0300

    feat(Classes.jsx): add to this page the BackButton component

--- vite/src/pages/Classes.jsx
+++ vite/src/pages/Classes.jsx
@@ -3,17 +3,19 @@ import ClassCard from "../components/ClassCard.jsx";
 import variables from "../utils/variables.js";
 import "../assets/classes.css";
 import NavbarMenu from "../components/NavbarMenu.jsx";
+import BackButton from "../components/BackButton.jsx";
 
 const Classes = () => {
   return (
     <>
-      <NavbarMenu
-        items={[
-          { item: "Home", url: "/" },
-          { item: "Follow Me", url: "/FollowMe" },
-        ]}
-      />
       <main className="fade-in">
+        <NavbarMenu
+          items={[
+            { item: "Home", url: "/" },
+            { item: "Follow Me", url: "/FollowMe" },
+          ]}
+        />
+        <BackButton url={"/FollowMe"} />
         <h1 className="follow-title">Classes</h1>
         <p className="description">Here you will find all the classes</p>
         <div className="classes-container">
