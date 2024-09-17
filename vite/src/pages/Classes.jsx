c27f03d963f01ffede0a12e1ab3a6df323fb4dca
tomikorzu <tomykorzu@icloud.com>
2024-09-17 10:23:45 -0300

    fix(navbar.css / classes.jsx): quit script of the classes-container in classes.jsx and change color / add animation on hover of navbar buttons

--- vite/src/pages/Classes.jsx
+++ vite/src/pages/Classes.jsx
@@ -17,21 +17,16 @@ const Classes = () => {
         <h1 className="follow-title">Classes</h1>
         <p className="description">Here you will find all the classes</p>
         <div className="classes-container">
-          {Array.isArray(variables.classesCards) &&
-          variables.classesCards.length > 0 ? (
-            variables.classesCards.map((classItem) => (
-              <ClassCard
-                key={classItem.title}
-                title={classItem.title}
-                description={classItem.description}
-                content={classItem.content.map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
-              />
-            ))
-          ) : (
-            <p>No classes available</p>
-          )}
+          {variables.classesCards.map((classItem) => (
+            <ClassCard
+              key={classItem.title}
+              title={classItem.title}
+              description={classItem.description}
+              content={classItem.content.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            />
+          ))}
         </div>
       </main>
     </>
